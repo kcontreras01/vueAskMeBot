ae1ea2be6b415023185fcb840e5e632de37a313c
Kiara Contreras <kiara@hashtag-labs.com>
2018-04-26 17:33:18 -0400

    basis

---
+++ main.js
@@ -0,0 +1,37 @@
+var watchExampleVM = new Vue({
+  el: '#watch-example',
+  data: {
+    question: '',
+    answer: 'I cannot give you an answer until you ask a question!'
+  },
+  watch: {
+    // watch allows async operation (accessing the API), limit how often we perform that operation, 
+    // and set intermediary states until we get a final answer
+    question: function (newQuestion, oldQuestion) {
+      this.answer = "Waiting for you to stop typing..."
+      this.getAnswer()
+    }
+  },
+  methods: {
+    getAnswer: _.debounce(
+      function(){
+        if (this.question.indexOf('?') === -1) {
+          this.answer = 'Questions usually contain a question mark. ;-)'
+          return
+        }
+        this.answer = 'Thinking...'
+        var vm = this
+        axios.get('https://yesno.wtf/api')
+          .then(function (response) {
+            vm.answer = _.capitalize(response.data.answer)
+          })
+          .catch(function (error) {
+            vm.answer = 'Error! Could not reach the API. ' + error
+          })
+      },
+      // This is the number of milliseconds we wait for the
+      // user to stop typing.
+      500
+    )
+  }
+})
